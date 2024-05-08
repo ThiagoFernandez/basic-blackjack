07d24270b3a64c7f077f0bfe939e47a76ecae90a
ThiagoFernandez <86986189+ThiagoFernandez@users.noreply.github.com>
2024-05-08 17:41:14 -0300

    error fixed, thx god

--- js/index.js
+++ js/index.js
@@ -1,44 +1,44 @@
+let firstCard = 10;
+let secondCard = 4;
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackjack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.querySelector("#message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
 
-
-let firstCard = 10
-let secondCard = 4
-let cards =[firstCard, secondCard]
-let sum = firstCard + secondCard
-let hasBlackjack= false
-let isAlive = true
-let message = ""
-let messageEl = document.querySelector("#message-el")
-let sumEl = document.querySelector("#sum-el")
-let cardsEl = document.querySelector("#cards-el")
-function renderGame(){
-    if (sum<=20){
-        message="do u want a new card?"
-    }
-    
-    else if(sum==21){
-        message="blackjack!"
-        hasBlackjack = true
-    }
-    
-    else {
-        message="u r out rookie!"
-        isAlive = false
-    }
-    cardsEl.textContent= "cards: "
-    for (let i=0; i=cards.length; i++) {
-        cardsEl.textContent += cards[i]
-    }
-    messageEl.textContent = message
-    sumEl.textContent = "sum: "+sum
-}
 function startGame() {
-    renderGame()
+    renderGame();
 }
-function newCard(){
-    let card = 6
-    sum += card
-    cards.push(card)
-    renderGame()
+
+function renderGame() {
+    messageEl.textContent = message;
+    sumEl.textContent = "Sum: " + sum;
+    // Clear cards element before re-rendering
+    cardsEl.textContent = "Cards: ";
+
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
+
+    if (sum <= 20) {
+        message = "Do you want a new card?";
+    } else if (sum === 21) {
+        message = "Blackjack!";
+        hasBlackjack = true;
+    } else {
+        message = "You are out, rookie!";
+        isAlive = false;
+    }
 }
-    
+
+function newCard() {
+    let card = 6;
+    sum += card;
+    cards.push(card);
+    renderGame();
+}
+
 
